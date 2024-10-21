762795072382e21a6ca4beb82d29c94db2c3b2ca
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-10-21 14:41:20 +0530

    Express Working

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+// Express Check
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// // Middleware (optional)
+// app.use(express.json());
+
+// // Define a simple route
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
 // FrameWork 
 // Import express
 const express = require('express');
@@ -9,5 +27,7 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+
