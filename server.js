da9dc645d1b8439ba1160a84616877b59f54693e
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-10-21 14:32:56 +0530

    framework used

---
+++ server.js
@@ -0,0 +1,13 @@
+// FrameWork 
+// Import express
+const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cors())
