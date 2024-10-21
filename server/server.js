ee5d8c918dd95ae00522b81bde9635a3333489ea
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-10-21 15:40:11 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -1,50 +1,47 @@
 // Express Check
-const express = require('express');
-const app = express();
-const PORT = 3000;
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
 
-// Middleware (optional)
-app.use(express.json());
+// // Middleware (optional)
+// app.use(express.json());
 
-// Define a simple route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// // Define a simple route
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
 
-// Start the server
-app.listen(PORT, () => {
+// // Start the server
+// app.listen(PORT, () => {
 
-
-
-  
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 // FrameWork 
 // Import express
-// const express = require('express');
-// const connectDb = require("./config/dbConnection");
-// const errorHandler = require("./middlewares/errorHandler");
-// const cors = require("cors");
+const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
-// connectDb();
-// const app = express();
-// const port = process.env.PORT || 5000;
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// // Error Handling Middleware
-// app.use(errorHandler);
+// Error Handling Middleware
+app.use(errorHandler);
 
 
-// // Routes Below
-// app.get('/',(req,res)=>{
-//     res.send("Working");
-// })
+// Routes Below
+app.get('/',(req,res)=>{
+    res.send("Working");
+})
 
-// // App Config Start 
-// app.listen(port,()=>{
-//     console.log('Server running on port http://localhost:${port}');
-// });
+// App Config Start 
+app.listen(port,()=>{
+    console.log('Server running on port http://localhost:${port}');
+});
 
