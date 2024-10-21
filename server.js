8a4bc3699a4c19c9b12545575635149333a42de9
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-10-21 15:05:19 +0530

    error Handling,  .get function , .listen

--- server.js
+++ server.js
@@ -29,5 +29,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+// Error Handling Middleware
+app.use(errorHandler);
 
+
+// Routes Below
+app.get('/',(req,res)=>{
+    res.send("Working");
+})
+
+// App Config Start 
+app.listen(port,()=>{
+    console.log('Server running on port http://localhost:${port}');
+});
 
