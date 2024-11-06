a778b9dbc768bb96d84778523c5ce52d820656a1
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2024-11-06 13:21:51 +0530

    Login user

--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    registerUser //,
-    // loginUser
+    registerUser ,
+    loginUser
 } = require("../controllers/userController");
 
 
